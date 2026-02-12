eb8972acef1844ac4e0ed000a5256007f9957808
neerayel <andrei11shum@mail.ru>
2026-02-12 22:49:41 +0300

    MidLogic interfaces structure update

--- Avtobus1ru_Test.MidLogic/Interfaces/ILinkService.cs
+++ Avtobus1ru_Test.MidLogic/Interfaces/ILinkService.cs
@@ -2,8 +2,13 @@
 
 namespace Avtobus1ru_Test.MidLogic.Interfaces
 {
-    public interface ILinkService : IService<LinkModel>
+    public interface ILinkService
     {
-        Task<List<LinkModel>> GetLongFromShortAsync(string shortURL);
+        Task<LinkModel> CreateAsync(string longURL);
+        Task<List<LinkModel>> GetAllAsync();
+        Task<LinkModel> GetByIdAsync(int id);
+        Task<LinkModel> GetLongFromShortAsync(string shortURL);
+        Task UpdateAsync(LinkModel item);
+        Task DeleteAsync(int id);
     }
 }
